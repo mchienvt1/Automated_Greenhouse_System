e3fc25b983c59de8b917e2aa4ba3b783c74e0414
Khang Nguyen <khanghoai1603@gmail.com>
2024-04-10 22:07:25 +0700

    add login page sign up page

--- Automated_Greenhouse_System/src/components/utils/env.jsx
+++ Automated_Greenhouse_System/src/components/utils/env.jsx
@@ -1,2 +1,2 @@
-export const username = 'hoang349'
-export const key = 'aio_MUyk45JjjGN0561KA3HOFMbkwqrh'
+// export const username = 'hoang349'
+// export const key = 'aio_MUyk45JjjGN0561KA3HOFMbkwqrh'
